63245855ae2df969cb72605bfda5e9854e3b93c3
Matias Giachello <matiasgiachello04@gmail.com>
2023-08-22 14:33:35 -0300

    nuevo push

--- tablas/src/components/DetallesEmpleados/DetallesEmpleados.jsx
+++ tablas/src/components/DetallesEmpleados/DetallesEmpleados.jsx
@@ -21,6 +21,61 @@ export default DetallesEmpleado;
 
 
 
+// import React, { useState } from 'react';
+// import './DetallesEmpleados.css'
+
+// const Detalles = () => {
+//   const [selectedItem, setSelectedItem] = useState(null);
+
+//   const items = [
+//     { id: 1, name: 'Item 1', description: 'Descripción del Item 1...' },
+//     { id: 2, name: 'Item 2', description: 'Descripción del Item 2...' },
+//     // Agrega más elementos aquí
+//   ];
+
+//   const handleItemClick = (item) => {
+//     setSelectedItem(item);
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Página Principal</h1>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>ID</th>
+//             <th>Nombre</th>
+//             <th>Acciones</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {items.map((item) => (
+//             <tr key={item.id}>
+//               <td>{item.id}</td>
+//               <td>{item.name}</td>
+//               <td>
+//                 <button onClick={() => handleItemClick(item)}>Ver detalles</button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+
+//       {selectedItem && (
+//         <div className="description-section">
+//           <h2>Detalles de {selectedItem.name}</h2>
+//           <p>{selectedItem.description}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Detalles;
+
+
+
+
 
 
 
