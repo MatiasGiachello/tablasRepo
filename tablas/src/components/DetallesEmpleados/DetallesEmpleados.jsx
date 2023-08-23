a1e09971a6bad05078d90c3132d0801a279c85b0
Matias Giachello <matiasgiachello04@gmail.com>
2023-08-23 15:57:39 -0300

    ..

--- tablas/src/components/DetallesEmpleados/DetallesEmpleados.jsx
+++ tablas/src/components/DetallesEmpleados/DetallesEmpleados.jsx
@@ -3,14 +3,23 @@ import "./DetallesEmpleados.css"
 
 const DetallesEmpleado = ({ empleado, onClose }) => {
   return (
-    <div className="detalles-container">
-      <h2>Detalles del Empleado</h2>
-      <p>Nombre: {empleado.name}</p>
-      <p>TimeStamp: {empleado.timeStamp}</p>
-      <h5>Evento: {empleado.motivo}</h5>
-      <p>Sistema: {empleado.sistema}</p>
-      <p>Tipo: {empleado.tipo}</p>
-      <button onClick={onClose}>Cerrar Detalles</button>
+    <div>
+      <div className="detalles-container">
+        <h2>Detalles del Empleado</h2>
+        <p>Nombre: {empleado.name}</p>
+        <p>TimeStamp: {empleado.timeStamp}</p>
+        <h5>Evento: {empleado.motivo}</h5>
+        <p>Sistema: {empleado.sistema}</p>
+        <p>Tipo: {empleado.tipo}</p>   
+        <div className="detalles-container">
+          <p>ID: {empleado.id}</p>
+          <p>Fecha: {empleado.timeStamp}</p>
+          <p>Titulo: {empleado.Titulo}</p>
+          <br /><br /><br /> <br /><br /> <br /> <br /><br /><br /> <br /> <br /> <br /><br /><br />                  
+          <button onClick={onClose}>Cerrar Detalles</button>
+        </div>
+      </div>
+
     </div>
 
   );
